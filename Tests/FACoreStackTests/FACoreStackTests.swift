cb998ad7ef4a0fcfb3539d74f6a11e776dd5f172
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2020-01-21 21:29:19 +0300

    Test case updates

--- Tests/FACoreStackTests/FACoreStackTests.swift
+++ Tests/FACoreStackTests/FACoreStackTests.swift
@@ -6,7 +6,7 @@ final class FACoreStackTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(FACoreStack().text, "Hello, World!")
+//        XCTAssertEqual(FACoreStack().text, "Hello, World!")
     }
 
     static var allTests = [

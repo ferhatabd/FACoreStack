d592f73afdf0a718e96424a4abdadb1b3b13584b
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2020-01-22 10:57:19 +0300

    tvOS support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "FACoreStack",
-    platforms: [.iOS(.v10) ],
+    platforms: [.iOS(.v10),
+                .tvOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
